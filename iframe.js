befdf545d590c8f639efb2056638d69c57e2b064
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-01-23 17:19:55 +0100

    chore(iframe html): remove html iframe (replaced by react application)

--- iframe.js
+++
@@ -1,9 +0,0 @@
-// Get height of the main element in the iframe document
-const documentHeight = document.getElementsByClassName("main-element")[0]
-  .scrollHeight;
-
-// Add some unique identifier to the string being passed
-const message = "documentHeight:" + documentHeight;
-
-// Pass message to (any*) parent document
-parent.postMessage(message, "*");
