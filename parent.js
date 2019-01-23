c7f4e10bd621d4795541185b9bb8f9c6ea3e89d6
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-01-23 17:15:49 +0100

    feat(parent.js): clean host javascript removing legacy eventListener

--- parent.js
+++ parent.js
@@ -1,21 +1,13 @@
 // Add event listener for messages being massed from the iframe
-const eventMethod = window.addEventListener
-  ? "addEventListener"
-  : "attachEvent";
-const eventer = window[eventMethod];
-const messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+const setHeight = e => {
+  // Check that message being passed is the documentHeight
+  if (typeof e.data === "string" && e.data.indexOf("documentHeight") > -1) {
+    const height = e.data.split("documentHeight:")[1];
+    const heightWithPadding = parseInt(height, 10) + 50; // add a bit of extra space
 
-eventer(
-  messageEvent,
-  function(e) {
-    // Check that message being passed is the documentHeight
-    if (typeof e.data === "string" && e.data.indexOf("documentHeight") > -1) {
-      const height = e.data.split("documentHeight:")[1];
-      const heightWithPadding = parseInt(height) + 50; // add a bit of extra space
+    // Change height of iframe
+    document.getElementById("myIframe").height = `${heightWithPadding}px`;
+  }
+};
 
-      // Change height of iframe
-      document.getElementById("myIframe").height = heightWithPadding + "px";
-    }
-  },
-  false
-);
+window.addEventListener("message", setHeight);
